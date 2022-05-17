6f13c3c294bb96faeb08e0a96c0e4ae6c6d00067
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-17 19:37:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vue Upload Image with Axios and Preview example
+Build Vue.js Image Upload with Preview to Server example using Axios for HTTP post FormData with progress bar.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="container" style="width:600px">
+      <div class="my-4">
+        <p id="devname"><span>Amica</span></p>
+        <h4>Vue.js upload Image with Preview</h4>
+      </div>
+      <upload-image></upload-image>
+    </div>
+	<footer-page></footer-page>
+  </div>
+</template>
+
+<script>
+import UploadImage from "./components/UploadImage";
+import FooterPage from './components/Footer';
+
+export default {
+  name: "App",
+  components: {
+    UploadImage,
+	FooterPage
+  }
+};
+</script>
+<style>
+#devname span{ background-color:#000000; color:#FFFFFF; width:100px; height:100px; padding:20px 10px; border-radius:50%; font-size:15px; }
+</style>
